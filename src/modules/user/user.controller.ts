e2704e58eb774c77838aaf0cf7fb02d45bb86f3c
Matheus Baraldi <matheushbaraldi@gmail.com>
2025-02-14 15:46:44 -0300

    feat: add swagger docs on routes

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -8,82 +8,104 @@ import {
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/role.guard';
-import { Roles } from '../auth/decorators/roles.decorator';
-import { Role } from './entity/user.entity';
+
 import {
-  HasPaginationQuery,
-  Pagination,
-  PrismaPaginationParams,
-} from '../auth/decorators/pagination.decorator';
+  FindAllUserResponse,
+  UserFilterParams,
+} from './dtos/find-all-user.dto';
+import { Role } from './enums/role.enum';
+import { ApiBadRequestExceptionReponse } from '@/common/decorators/exceptions/bad-request-response.decorator';
+import { ApiUnauthorizedExceptionReponse } from '@/common/decorators/exceptions/unauthorized-response.decorator';
+import { ApiForbiddenExceptionReponse } from '@/common/decorators/exceptions/forbidden-response.decorator';
+import { ApiNotFoundExceptionReponse } from '@/common/decorators/exceptions/not-found-response.decorator';
+import { User } from './entity/user.entity';
+import { Roles } from '@/common/decorators/roles/roles.decorator';
+import { NoRoles } from '@/common/decorators/roles/no-roles.decorator';
 import {
   RequestUser,
   UserFromRequest,
-} from '../auth/decorators/user-from-request.decorator';
-import { NoRoles } from '../auth/decorators/no-roles.decorator';
+} from '@/common/decorators/user/user-from-request.decorator';
+
+const route = '/users';
+const routeId = '/users/:id';
 
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Roles(Role.ADMIN, Role.USER) // Deixei a role user aqui pra facilitar o uso da Api, porém, o ideal é que USER não tenha acesso a esses endpoints
 @ApiTags('users')
-@Controller('users')
+@Controller({ path: route, version: '1' })
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: User })
+  @ApiUnauthorizedExceptionReponse(route)
+  @ApiForbiddenExceptionReponse(route)
+  @ApiBadRequestExceptionReponse(route)
   async create(@Body() createUserDto: CreateUserDto) {
-    const createdUser = await this.userService.create(createUserDto);
-    return {
-      statusCode: 201,
-      message: 'User created successfully',
-      user: createdUser,
-    };
+    return this.userService.create(createUserDto);
   }
 
   @Get()
-  @HasPaginationQuery()
-  findAll(@Pagination() pagination: PrismaPaginationParams) {
-    return this.userService.findAll(pagination);
+  @ApiOkResponse({ type: FindAllUserResponse })
+  @ApiUnauthorizedExceptionReponse(route)
+  @ApiForbiddenExceptionReponse(route)
+  findAll(@Query() params: UserFilterParams) {
+    return this.userService.findAll(params);
   }
 
-  @Get('id/:id')
+  @Get(':id')
+  @ApiOkResponse({ type: User })
+  @ApiUnauthorizedExceptionReponse(routeId)
+  @ApiForbiddenExceptionReponse(routeId)
+  @ApiNotFoundExceptionReponse(routeId, 'User')
   async findOne(@Param('id') id: string) {
-    const user = await this.userService.findOne(id);
-    return {
-      user,
-    };
+    return this.userService.findOne(id);
   }
 
-  @Patch('id/:id')
+  @Patch(':id')
+  @ApiCreatedResponse({ type: FindAllUserResponse })
+  @ApiUnauthorizedExceptionReponse(routeId)
+  @ApiForbiddenExceptionReponse(routeId)
+  @ApiNotFoundExceptionReponse(routeId, 'User')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    const updatedUser = await this.userService.update(id, updateUserDto);
-    return {
-      user: updatedUser,
-    };
+    return this.userService.update(id, updateUserDto);
   }
 
   @NoRoles()
   @Patch('update/me')
+  @ApiCreatedResponse({ type: User })
+  @ApiBadRequestExceptionReponse(route)
+  @ApiUnauthorizedExceptionReponse(route)
   async updateMe(
     @UserFromRequest() user: RequestUser,
     @Body() updateUserDto: UpdateUserDto,
   ) {
-    const updatedUser = await this.userService.update(user.id, updateUserDto);
-    return {
-      user: updatedUser,
-    };
+    return this.userService.update(user.id, updateUserDto);
   }
 
-  @Delete('id/:id')
+  @Delete(':id')
+  @ApiUnauthorizedExceptionReponse(routeId)
+  @ApiForbiddenExceptionReponse(routeId)
+  @ApiNotFoundExceptionReponse(routeId, 'User')
+  @ApiNoContentResponse({ description: 'User deleted successfully' })
   @HttpCode(HttpStatus.NO_CONTENT)
   async delete(@Param('id') id: string) {
-    await this.userService.delete(id);
+    return this.userService.delete(id);
   }
 }
