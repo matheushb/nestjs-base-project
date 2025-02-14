fc7c24c578a69b3a8cd8b699c1e8fbe29b63dd1e
Matheus Baraldi <matheushbaraldi@gmail.com>
2025-02-14 15:38:32 -0300

    feat: add logger on http-exception-filter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -1,8 +1,10 @@
+import { UserFromJwt } from '@/modules/auth/dtos/user-from-jwt.dto';
 import {
   ExceptionFilter,
   Catch,
   ArgumentsHost,
   HttpException,
+  Logger,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
@@ -12,13 +14,23 @@ interface HttpExceptionMessage extends HttpException {
   };
 }
 
+type RequestWithUser = Request & { user: UserFromJwt };
+
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger('Exception');
+
   catch(exception: HttpExceptionMessage, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+    const request = ctx.getRequest<RequestWithUser>();
     const status = exception.getStatus();
+
+    const { method, url } = request;
+
+    this.logger.error(
+      `method=${method} path="${url}" status=${status} user="${request?.user?.email ?? 'anonymous'}"`,
+    );
 
     response.status(status).json({
       message: exception.getResponse().message ?? exception.message,
